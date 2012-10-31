django/django

Fixed #17083 -- Allowed sessions to use non-default cache.

django/contrib/sessions/tests.py
@@ -13,8 +13,8 @@ from django.contrib.sessions.backends.file import SessionStore as FileSession
 from django.contrib.sessions.backends.signed_cookies import SessionStore as CookieSession
 from django.contrib.sessions.models import Session
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.core.cache import get_cache
 from django.core import management
-from django.core.cache import DEFAULT_CACHE_ALIAS
 from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.http import HttpResponse
 from django.test import TestCase, RequestFactory
@@ -136,8 +136,8 @@ class SessionTestsMixin(object):
         self.assertTrue(self.session.modified)
 
     def test_save(self):
-        if (hasattr(self.session, '_cache') and
-                'DummyCache' in settings.CACHES[DEFAULT_CACHE_ALIAS]['BACKEND']):
+        if (hasattr(self.session, '_cache') and'DummyCache' in
+            settings.CACHES[settings.SESSION_CACHE_ALIAS]['BACKEND']):
             raise unittest.SkipTest("Session saving tests require a real cache backend")
         self.session.save()
         self.assertTrue(self.session.exists(self.session.session_key))
@@ -355,7 +355,8 @@ class CacheDBSessionTests(SessionTestsMixin, TestCase):
 
     backend = CacheDBSession
 
-    @unittest.skipIf('DummyCache' in settings.CACHES[DEFAULT_CACHE_ALIAS]['BACKEND'],
+    @unittest.skipIf('DummyCache' in
+        settings.CACHES[settings.SESSION_CACHE_ALIAS]['BACKEND'],
         "Session saving tests require a real cache backend")
     def test_exists_searches_cache_first(self):
         self.session.save()
@@ -453,6 +454,19 @@ class CacheSessionTests(SessionTestsMixin, unittest.TestCase):
             warnings.simplefilter("ignore")
             self.session._session_key = (string.ascii_letters + string.digits) * 20
             self.assertEqual(self.session.load(), {})
+
+    @override_settings(CACHES={
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        },
+        'session': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        },
+    }, SESSION_CACHE_ALIAS='session')
+    def test_non_default_cache(self):
+        self.session.save()
+        self.assertEqual(get_cache('default').get(self.session.session_key), None)
+        self.assertNotEqual(get_cache('sessions').get(self.session.session_key), None)
 
 
 class SessionMiddlewareTests(unittest.TestCase):

senior106/Django-Project

django/contrib/sessions/tests.py
@@ -13,8 +13,8 @@ from django.contrib.sessions.backends.file import SessionStore as FileSession
 from django.contrib.sessions.backends.signed_cookies import SessionStore as CookieSession
 from django.contrib.sessions.models import Session
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.core.cache import get_cache
 from django.core import management
-from django.core.cache import DEFAULT_CACHE_ALIAS
 from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.http import HttpResponse
 from django.test import TestCase, RequestFactory
@@ -136,8 +136,8 @@ class SessionTestsMixin(object):
         self.assertTrue(self.session.modified)
 
     def test_save(self):
-        if (hasattr(self.session, '_cache') and
-                'DummyCache' in settings.CACHES[DEFAULT_CACHE_ALIAS]['BACKEND']):
+        if (hasattr(self.session, '_cache') and'DummyCache' in
+            settings.CACHES[settings.SESSION_CACHE_ALIAS]['BACKEND']):
             raise unittest.SkipTest("Session saving tests require a real cache backend")
         self.session.save()
         self.assertTrue(self.session.exists(self.session.session_key))
@@ -355,7 +355,8 @@ class CacheDBSessionTests(SessionTestsMixin, TestCase):
 
     backend = CacheDBSession
 
-    @unittest.skipIf('DummyCache' in settings.CACHES[DEFAULT_CACHE_ALIAS]['BACKEND'],
+    @unittest.skipIf('DummyCache' in
+        settings.CACHES[settings.SESSION_CACHE_ALIAS]['BACKEND'],
         "Session saving tests require a real cache backend")
     def test_exists_searches_cache_first(self):
         self.session.save()
@@ -453,6 +454,23 @@ class CacheSessionTests(SessionTestsMixin, unittest.TestCase):
             warnings.simplefilter("ignore")
             self.session._session_key = (string.ascii_letters + string.digits) * 20
             self.assertEqual(self.session.load(), {})
+
+    def test_default_cache(self):
+        self.session.save()
+        self.assertNotEqual(get_cache('default').get(self.session.cache_key), None)
+
+    @override_settings(CACHES={
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        },
+        'sessions': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        },
+    }, SESSION_CACHE_ALIAS='sessions')
+    def test_non_default_cache(self):
+        self.session.save()
+        self.assertEqual(get_cache('default').get(self.session.cache_key), None)
+        self.assertNotEqual(get_cache('sessions').get(self.session.cache_key), None)
 
 
 class SessionMiddlewareTests(unittest.TestCase):
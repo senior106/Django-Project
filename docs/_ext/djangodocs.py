django/django

commit bacdfbf3d1eb23ff8e8110681728a5b467443446

Removed deprecated html_translator_class sphinx config option.

docs/_ext/djangodocs.py
@@ -63,6 +63,7 @@ def setup(app):
                  man=(visit_snippet_literal, depart_snippet_literal),
                  text=(visit_snippet_literal, depart_snippet_literal),
                  texinfo=(visit_snippet_literal, depart_snippet_literal))
+    app.set_translator('djangohtml', DjangoHTMLTranslator)
     return {'parallel_read_safe': True}
 
 

docs/conf.py
@@ -192,9 +192,6 @@ html_last_updated_fmt = '%b %d, %Y'
 # typographically correct entities.
 html_use_smartypants = True
 
-# HTML translator class for the builder
-html_translator_class = "djangodocs.DjangoHTMLTranslator"
-
 # Content template for the index page.
 # html_index = ''
 

senior106/Django-Project

commit a10295bf053e00c8687b950da1e2752a71c694a4

Fixed missing versionadded/changed annotations on docs.djangoproject.com.
Fixed a regression in bacdfbf3d1eb23ff8e8110681728a5b467443446.

docs/_ext/djangodocs.py
@@ -64,6 +64,7 @@ def setup(app):
                  text=(visit_snippet_literal, depart_snippet_literal),
                  texinfo=(visit_snippet_literal, depart_snippet_literal))
     app.set_translator('djangohtml', DjangoHTMLTranslator)
+    app.set_translator('json', DjangoHTMLTranslator)
     return {'parallel_read_safe': True}
 
 
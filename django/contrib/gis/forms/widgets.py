django/django

commit 2ebfda38e65177dfc7d6e90457ffa4a0fa2796f0

Fixed #25004 -- Updated OpenLayers-based widget to OpenLayers 3
Thanks Tim Graham for the review.

django/contrib/gis/forms/widgets.py
@@ -80,13 +80,22 @@ class OpenLayersWidget(BaseGeometryWidget):
     template_name = 'gis/openlayers.html'
 
     class Media:
+        css = {
+            'all': (
+                'https://cdnjs.cloudflare.com/ajax/libs/ol3/3.20.1/ol.css',
+                'gis/css/ol3.css',
+            )
+        }
         js = (
-            'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/ol3/3.20.1/ol.js',
             'gis/js/OLMapWidget.js',
         )
 
+    def serialize(self, value):
+        return value.json if value else ''
 
-class OSMWidget(BaseGeometryWidget):
+
+class OSMWidget(OpenLayersWidget):
     """
     An OpenLayers/OpenStreetMap-based widget.
     """
@@ -94,12 +103,6 @@ class OSMWidget(BaseGeometryWidget):
     default_lon = 5
     default_lat = 47
     map_srid = 3857
-
-    class Media:
-        js = (
-            'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',
-            'gis/js/OLMapWidget.js',
-        )
 
     def __init__(self, attrs=None):
         super(OSMWidget, self).__init__()

tests/gis_tests/test_geoforms.py
@@ -204,7 +204,7 @@ class SpecializedFieldTest(SimpleTestCase):
 
         self.assertIn('<textarea ', rendered)
         self.assertIn('required', rendered)
-        self.assertIn(geom.wkt, rendered)
+        self.assertIn(escape(geom.json), rendered)
 
     # map_srid in operlayers.html template must not be localized.
     @override_settings(USE_L10N=True, USE_THOUSAND_SEPARATOR=True)
@@ -318,7 +318,7 @@ class OSMWidgetTest(SimpleTestCase):
         form = PointForm(data={'p': geom})
         rendered = form.as_p()
 
-        self.assertIn("OpenStreetMap (Mapnik)", rendered)
+        self.assertIn("ol.source.OSM()", rendered)
         self.assertIn("id: 'id_p',", rendered)
 
     def test_default_lat_lon(self):

django/contrib/gis/static/gis/css/ol3.css
@@ -1,0 +1,31 @@
+.switch-type {
+    background-repeat: no-repeat;
+    cursor: pointer;
+    top: 0.5em;
+    width: 22px;
+    height: 20px;
+}
+
+.type-Point {
+    background-image: url("../img/draw_point_off.png");
+    right: 5px;
+}
+.type-Point.type-active {
+    background-image: url("../img/draw_point_on.png");
+}
+
+.type-LineString {
+    background-image: url("../img/draw_line_off.png");
+    right: 30px;
+}
+.type-LineString.type-active {
+    background-image: url("../img/draw_line_on.png");
+}
+
+.type-Polygon {
+    background-image: url("../img/draw_polygon_off.png");
+    right: 55px;
+}
+.type-Polygon.type-active {
+    background-image: url("../img/draw_polygon_on.png");
+}

senior106/Django-Project

commit 3cc314a8e2d50710f4ed042d19db1c123cdc0b86

Fixed #27939 -- Updated OpenLayersWidget.map_srid for OpenLayers 3.
Regression in 2ebfda38e65177dfc7d6e90457ffa4a0fa2796f0 (refs #25004).

django/contrib/gis/forms/widgets.py
@@ -76,6 +76,7 @@ class BaseGeometryWidget(Widget):
 
 class OpenLayersWidget(BaseGeometryWidget):
     template_name = 'gis/openlayers.html'
+    map_srid = 3857
 
     class Media:
         css = {
@@ -100,7 +101,6 @@ class OSMWidget(OpenLayersWidget):
     template_name = 'gis/openlayers-osm.html'
     default_lon = 5
     default_lat = 47
-    map_srid = 3857
 
     def __init__(self, attrs=None):
         super().__init__()

tests/gis_tests/test_geoforms.py
@@ -116,7 +116,7 @@ class GeometryFieldTest(SimpleTestCase):
         # ordering of the rendered dictionary.
         pt1_serialized = re.search(r'<textarea [^>]*>({[^<]+})<', output).groups()[0]
         pt1_json = json.loads(pt1_serialized.replace('&quot;', '"'))
-        self.assertEqual(pt1_json, {'coordinates': [7.3, 44.0], 'type': 'Point'})
+        self.assertEqual(pt1_json, {'coordinates': [812632.2827908975, 5465442.183322753], 'type': 'Point'})
 
         self.assertInHTML(
             '<textarea id="id_pt2" class="vSerializedField required" cols="150"'
@@ -200,7 +200,7 @@ class SpecializedFieldTest(SimpleTestCase):
         self.assertTrue(form_instance.is_valid())
         rendered = form_instance.as_p()
         self.assertIn('new MapWidget(options);', rendered)
-        self.assertIn('map_srid: 4326,', rendered)
+        self.assertIn('map_srid: 3857,', rendered)
         self.assertIn('gis/js/OLMapWidget.js', str(form_instance.media))
 
     def assertTextarea(self, geom, rendered):
@@ -208,7 +208,9 @@ class SpecializedFieldTest(SimpleTestCase):
 
         self.assertIn('<textarea ', rendered)
         self.assertIn('required', rendered)
-        self.assertIn(escape(geom.json), rendered)
+        ogr = geom.ogr
+        ogr.transform(3857)
+        self.assertIn(escape(ogr.json), rendered)
 
     # map_srid in operlayers.html template must not be localized.
     @override_settings(USE_L10N=True, USE_THOUSAND_SEPARATOR=True)
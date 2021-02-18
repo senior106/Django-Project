django/django

commit bb0b6e526340e638522e093765e534df4e4393d2

Fixed #28211 -- Prevented ORing an empty Q() from reducing query join efficiency.

django/db/models/query_utils.py
@@ -5,6 +5,7 @@ Factored out from django.db.models.query to avoid making the main module very
 large and/or so that they can be used by other modules without getting into
 circular import difficulties.
 """
+import copy
 import functools
 import inspect
 from collections import namedtuple
@@ -61,6 +62,14 @@ class Q(tree.Node):
     def _combine(self, other, conn):
         if not isinstance(other, Q):
             raise TypeError(other)
+
+        # If the other Q() is empty, ignore it and just use `self`.
+        if not other:
+            return copy.deepcopy(self)
+        # Or if this Q is empty, ignore it and just use `other`.
+        elif not self:
+            return copy.deepcopy(other)
+
         obj = type(self)()
         obj.connector = conn
         obj.add(self, conn)

tests/queries/test_q.py
@@ -11,6 +11,14 @@ class QTests(SimpleTestCase):
     def test_combine_and_both_empty(self):
         self.assertEqual(Q() & Q(), Q())
 
+    def test_combine_or_empty(self):
+        q = Q(x=1)
+        self.assertEqual(q | Q(), q)
+        self.assertEqual(Q() | q, q)
+
+    def test_combine_or_both_empty(self):
+        self.assertEqual(Q() | Q(), Q())
+
     def test_deconstruct(self):
         q = Q(price__gt=F('discounted_price'))
         path, args, kwargs = q.deconstruct()

senior106/Django-Project

commit 34242ae4444f680bfaa7496a18d3018f95d5c45a

Fixed #32450 -- Fixed crash when ANDing/ORing an empty Q() with not pickleable Q().
Regression in bb0b6e526340e638522e093765e534df4e4393d2.

django/db/models/query_utils.py
@@ -5,7 +5,6 @@ Factored out from django.db.models.query to avoid making the main module very
 large and/or so that they can be used by other modules without getting into
 circular import difficulties.
 """
-import copy
 import functools
 import inspect
 from collections import namedtuple
@@ -46,10 +45,12 @@ class Q(tree.Node):
 
         # If the other Q() is empty, ignore it and just use `self`.
         if not other:
-            return copy.deepcopy(self)
+            _, args, kwargs = self.deconstruct()
+            return type(self)(*args, **kwargs)
         # Or if this Q is empty, ignore it and just use `other`.
         elif not self:
-            return copy.deepcopy(other)
+            _, args, kwargs = other.deconstruct()
+            return type(other)(*args, **kwargs)
 
         obj = type(self)()
         obj.connector = conn

tests/queries/test_q.py
@@ -8,11 +8,19 @@ class QTests(SimpleTestCase):
         self.assertEqual(q & Q(), q)
         self.assertEqual(Q() & q, q)
 
+        q = Q(x__in={}.keys())
+        self.assertEqual(q & Q(), q)
+        self.assertEqual(Q() & q, q)
+
     def test_combine_and_both_empty(self):
         self.assertEqual(Q() & Q(), Q())
 
     def test_combine_or_empty(self):
         q = Q(x=1)
+        self.assertEqual(q | Q(), q)
+        self.assertEqual(Q() | q, q)
+
+        q = Q(x__in={}.keys())
         self.assertEqual(q | Q(), q)
         self.assertEqual(Q() | q, q)
 
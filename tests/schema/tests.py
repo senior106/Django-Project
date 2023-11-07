django/django

commit 7414704e88d73dafbcfbb85f9bc54cb6111439d3

Fixed #470 -- Added support for database defaults on fields.
Special thanks to Hannes Ljungberg for finding multiple implementation
gaps.

Thanks also to Simon Charette, Adam Johnson, and Mariusz Felisiak for
reviews.

django/db/backends/base/schema.py
@@ -12,7 +12,7 @@ from django.db.backends.ddl_references import (
     Table,
 )
 from django.db.backends.utils import names_digest, split_identifier, truncate_name
-from django.db.models import Deferrable, Index
+from django.db.models import NOT_PROVIDED, Deferrable, Index
 from django.db.models.sql import Query
 from django.db.transaction import TransactionManagementError, atomic
 from django.utils import timezone
@@ -296,6 +296,12 @@ class BaseDatabaseSchemaEditor:
             yield self._comment_sql(field.db_comment)
         # Work out nullability.
         null = field.null
+        # Add database default.
+        if field.db_default is not NOT_PROVIDED:
+            default_sql, default_params = self.db_default_sql(field)
+            yield f"DEFAULT {default_sql}"
+            params.extend(default_params)
+            include_default = False
         # Include a default value, if requested.
         include_default = (
             include_default
@@ -399,6 +405,22 @@ class BaseDatabaseSchemaEditor:
         contain a '%s' placeholder for a default value.
         """
         return "%s"
+
+    def db_default_sql(self, field):
+        """Return the sql and params for the field's database default."""
+        from django.db.models.expressions import Value
+
+        sql = "%s" if isinstance(field.db_default, Value) else "(%s)"
+        query = Query(model=field.model)
+        compiler = query.get_compiler(connection=self.connection)
+        default_sql, params = compiler.compile(field.db_default)
+        if self.connection.features.requires_literal_defaults:
+            # Some databases doesn't support parameterized defaults (Oracle,
+            # SQLite). If this is the case, the individual schema backend
+            # should implement prepare_default().
+            default_sql %= tuple(self.prepare_default(p) for p in params)
+            params = []
+        return sql % default_sql, params
 
     @staticmethod
     def _effective_default(field):
@@ -1025,6 +1047,21 @@ class BaseDatabaseSchemaEditor:
             )
             actions.append(fragment)
             post_actions.extend(other_actions)
+
+        if new_field.db_default is not NOT_PROVIDED:
+            if (
+                old_field.db_default is NOT_PROVIDED
+                or new_field.db_default != old_field.db_default
+            ):
+                actions.append(
+                    self._alter_column_database_default_sql(model, old_field, new_field)
+                )
+        elif old_field.db_default is not NOT_PROVIDED:
+            actions.append(
+                self._alter_column_database_default_sql(
+                    model, old_field, new_field, drop=True
+                )
+            )
         # When changing a column NULL constraint to NOT NULL with a given
         # default value, we need to perform 4 steps:
         #  1. Add a default for new incoming writes
@@ -1033,7 +1070,11 @@ class BaseDatabaseSchemaEditor:
         #  4. Drop the default again.
         # Default change?
         needs_database_default = False
-        if old_field.null and not new_field.null:
+        if (
+            old_field.null
+            and not new_field.null
+            and new_field.db_default is NOT_PROVIDED
+        ):
             old_default = self.effective_default(old_field)
             new_default = self.effective_default(new_field)
             if (
@@ -1051,9 +1092,9 @@ class BaseDatabaseSchemaEditor:
             if fragment:
                 null_actions.append(fragment)
         # Only if we have a default and there is a change from NULL to NOT NULL
-        four_way_default_alteration = new_field.has_default() and (
-            old_field.null and not new_field.null
-        )
+        four_way_default_alteration = (
+            new_field.has_default() or new_field.db_default is not NOT_PROVIDED
+        ) and (old_field.null and not new_field.null)
         if actions or null_actions:
             if not four_way_default_alteration:
                 # If we don't have to do a 4-way default alteration we can
@@ -1074,15 +1115,20 @@ class BaseDatabaseSchemaEditor:
                     params,
                 )
             if four_way_default_alteration:
+                if new_field.db_default is NOT_PROVIDED:
+                    default_sql = "%s"
+                    params = [new_default]
+                else:
+                    default_sql, params = self.db_default_sql(new_field)
                 # Update existing rows with default value
                 self.execute(
                     self.sql_update_with_default
                     % {
                         "table": self.quote_name(model._meta.db_table),
                         "column": self.quote_name(new_field.column),
-                        "default": "%s",
+                        "default": default_sql,
                     },
-                    [new_default],
+                    params,
                 )
                 # Since we didn't run a NOT NULL change before we need to do it
                 # now
@@ -1260,6 +1306,34 @@ class BaseDatabaseSchemaEditor:
                 "column": self.quote_name(new_field.column),
                 "type": new_db_params["type"],
                 "default": default,
+            },
+            params,
+        )
+
+    def _alter_column_database_default_sql(
+        self, model, old_field, new_field, drop=False
+    ):
+        """
+        Hook to specialize column database default alteration.
+
+        Return a (sql, params) fragment to add or drop (depending on the drop
+        argument) a default to new_field's column.
+        """
+        if drop:
+            sql = self.sql_alter_column_no_default
+            default_sql = ""
+            params = []
+        else:
+            sql = self.sql_alter_column_default
+            default_sql, params = self.db_default_sql(new_field)
+
+        new_db_params = new_field.db_parameters(connection=self.connection)
+        return (
+            sql
+            % {
+                "column": self.quote_name(new_field.column),
+                "type": new_db_params["type"],
+                "default": default_sql,
             },
             params,
         )

tests/migrations/test_operations.py
@@ -1,14 +1,18 @@
+import math
+
 from django.core.exceptions import FieldDoesNotExist
 from django.db import IntegrityError, connection, migrations, models, transaction
 from django.db.migrations.migration import Migration
 from django.db.migrations.operations.fields import FieldOperation
 from django.db.migrations.state import ModelState, ProjectState
-from django.db.models.functions import Abs
+from django.db.models.expressions import Value
+from django.db.models.functions import Abs, Pi
 from django.db.transaction import atomic
 from django.test import (
     SimpleTestCase,
     ignore_warnings,
     override_settings,
+    skipIfDBFeature,
     skipUnlessDBFeature,
 )
 from django.test.utils import CaptureQueriesContext
@@ -1340,7 +1344,7 @@ class OperationTests(OperationTestBase):
         self.assertEqual(operation.describe(), "Add field height to Pony")
         self.assertEqual(operation.migration_name_fragment, "pony_height")
         project_state, new_state = self.make_test_state("test_adfl", operation)
-        self.assertEqual(len(new_state.models["test_adfl", "pony"].fields), 4)
+        self.assertEqual(len(new_state.models["test_adfl", "pony"].fields), 6)
         field = new_state.models["test_adfl", "pony"].fields["height"]
         self.assertEqual(field.default, 5)
         # Test the database alteration
@@ -1528,7 +1532,7 @@ class OperationTests(OperationTestBase):
         )
         new_state = project_state.clone()
         operation.state_forwards("test_adflpd", new_state)
-        self.assertEqual(len(new_state.models["test_adflpd", "pony"].fields), 4)
+        self.assertEqual(len(new_state.models["test_adflpd", "pony"].fields), 6)
         field = new_state.models["test_adflpd", "pony"].fields["height"]
         self.assertEqual(field.default, models.NOT_PROVIDED)
         # Test the database alteration
@@ -1547,6 +1551,169 @@ class OperationTests(OperationTestBase):
             sorted(definition[2]), ["field", "model_name", "name", "preserve_default"]
         )
 
+    def test_add_field_database_default(self):
+        """The AddField operation can set and unset a database default."""
+        app_label = "test_adfldd"
+        table_name = f"{app_label}_pony"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AddField(
+            "Pony", "height", models.FloatField(null=True, db_default=4)
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertEqual(len(new_state.models[app_label, "pony"].fields), 6)
+        field = new_state.models[app_label, "pony"].fields["height"]
+        self.assertEqual(field.default, models.NOT_PROVIDED)
+        self.assertEqual(field.db_default, Value(4))
+        project_state.apps.get_model(app_label, "pony").objects.create(weight=4)
+        self.assertColumnNotExists(table_name, "height")
+        # Add field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertColumnExists(table_name, "height")
+        new_model = new_state.apps.get_model(app_label, "pony")
+        old_pony = new_model.objects.get()
+        self.assertEqual(old_pony.height, 4)
+        new_pony = new_model.objects.create(weight=5)
+        if not connection.features.can_return_columns_from_insert:
+            new_pony.refresh_from_db()
+        self.assertEqual(new_pony.height, 4)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertColumnNotExists(table_name, "height")
+        # Deconstruction.
+        definition = operation.deconstruct()
+        self.assertEqual(definition[0], "AddField")
+        self.assertEqual(definition[1], [])
+        self.assertEqual(
+            definition[2],
+            {
+                "field": field,
+                "model_name": "Pony",
+                "name": "height",
+            },
+        )
+
+    def test_add_field_database_default_special_char_escaping(self):
+        app_label = "test_adflddsce"
+        table_name = f"{app_label}_pony"
+        project_state = self.set_up_test_model(app_label)
+        old_pony_pk = (
+            project_state.apps.get_model(app_label, "pony").objects.create(weight=4).pk
+        )
+        tests = ["%", "'", '"']
+        for db_default in tests:
+            with self.subTest(db_default=db_default):
+                operation = migrations.AddField(
+                    "Pony",
+                    "special_char",
+                    models.CharField(max_length=1, db_default=db_default),
+                )
+                new_state = project_state.clone()
+                operation.state_forwards(app_label, new_state)
+                self.assertEqual(len(new_state.models[app_label, "pony"].fields), 6)
+                field = new_state.models[app_label, "pony"].fields["special_char"]
+                self.assertEqual(field.default, models.NOT_PROVIDED)
+                self.assertEqual(field.db_default, Value(db_default))
+                self.assertColumnNotExists(table_name, "special_char")
+                with connection.schema_editor() as editor:
+                    operation.database_forwards(
+                        app_label, editor, project_state, new_state
+                    )
+                self.assertColumnExists(table_name, "special_char")
+                new_model = new_state.apps.get_model(app_label, "pony")
+                try:
+                    new_pony = new_model.objects.create(weight=5)
+                    if not connection.features.can_return_columns_from_insert:
+                        new_pony.refresh_from_db()
+                    self.assertEqual(new_pony.special_char, db_default)
+
+                    old_pony = new_model.objects.get(pk=old_pony_pk)
+                    if connection.vendor != "oracle" or db_default != "'":
+                        # The single quotation mark ' is properly quoted and is
+                        # set for new rows on Oracle, however it is not set on
+                        # existing rows. Skip the assertion as it's probably a
+                        # bug in Oracle.
+                        self.assertEqual(old_pony.special_char, db_default)
+                finally:
+                    with connection.schema_editor() as editor:
+                        operation.database_backwards(
+                            app_label, editor, new_state, project_state
+                        )
+
+    @skipUnlessDBFeature("supports_expression_defaults")
+    def test_add_field_database_default_function(self):
+        app_label = "test_adflddf"
+        table_name = f"{app_label}_pony"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AddField(
+            "Pony", "height", models.FloatField(db_default=Pi())
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertEqual(len(new_state.models[app_label, "pony"].fields), 6)
+        field = new_state.models[app_label, "pony"].fields["height"]
+        self.assertEqual(field.default, models.NOT_PROVIDED)
+        self.assertEqual(field.db_default, Pi())
+        project_state.apps.get_model(app_label, "pony").objects.create(weight=4)
+        self.assertColumnNotExists(table_name, "height")
+        # Add field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertColumnExists(table_name, "height")
+        new_model = new_state.apps.get_model(app_label, "pony")
+        old_pony = new_model.objects.get()
+        self.assertAlmostEqual(old_pony.height, math.pi)
+        new_pony = new_model.objects.create(weight=5)
+        if not connection.features.can_return_columns_from_insert:
+            new_pony.refresh_from_db()
+        self.assertAlmostEqual(old_pony.height, math.pi)
+
+    def test_add_field_both_defaults(self):
+        """The AddField operation with both default and db_default."""
+        app_label = "test_adflbddd"
+        table_name = f"{app_label}_pony"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AddField(
+            "Pony", "height", models.FloatField(default=3, db_default=4)
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertEqual(len(new_state.models[app_label, "pony"].fields), 6)
+        field = new_state.models[app_label, "pony"].fields["height"]
+        self.assertEqual(field.default, 3)
+        self.assertEqual(field.db_default, Value(4))
+        project_state.apps.get_model(app_label, "pony").objects.create(weight=4)
+        self.assertColumnNotExists(table_name, "height")
+        # Add field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertColumnExists(table_name, "height")
+        new_model = new_state.apps.get_model(app_label, "pony")
+        old_pony = new_model.objects.get()
+        self.assertEqual(old_pony.height, 4)
+        new_pony = new_model.objects.create(weight=5)
+        if not connection.features.can_return_columns_from_insert:
+            new_pony.refresh_from_db()
+        self.assertEqual(new_pony.height, 3)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertColumnNotExists(table_name, "height")
+        # Deconstruction.
+        definition = operation.deconstruct()
+        self.assertEqual(definition[0], "AddField")
+        self.assertEqual(definition[1], [])
+        self.assertEqual(
+            definition[2],
+            {
+                "field": field,
+                "model_name": "Pony",
+                "name": "height",
+            },
+        )
+
     def test_add_field_m2m(self):
         """
         Tests the AddField operation with a ManyToManyField.
@@ -1558,7 +1725,7 @@ class OperationTests(OperationTestBase):
         )
         new_state = project_state.clone()
         operation.state_forwards("test_adflmm", new_state)
-        self.assertEqual(len(new_state.models["test_adflmm", "pony"].fields), 4)
+        self.assertEqual(len(new_state.models["test_adflmm", "pony"].fields), 6)
         # Test the database alteration
         self.assertTableNotExists("test_adflmm_pony_stables")
         with connection.schema_editor() as editor:
@@ -1727,7 +1894,7 @@ class OperationTests(OperationTestBase):
         self.assertEqual(operation.migration_name_fragment, "remove_pony_pink")
         new_state = project_state.clone()
         operation.state_forwards("test_rmfl", new_state)
-        self.assertEqual(len(new_state.models["test_rmfl", "pony"].fields), 2)
+        self.assertEqual(len(new_state.models["test_rmfl", "pony"].fields), 4)
         # Test the database alteration
         self.assertColumnExists("test_rmfl_pony", "pink")
         with connection.schema_editor() as editor:
@@ -1933,6 +2100,146 @@ class OperationTests(OperationTestBase):
         self.assertEqual(definition[0], "AlterField")
         self.assertEqual(definition[1], [])
         self.assertEqual(sorted(definition[2]), ["field", "model_name", "name"])
+
+    def test_alter_field_add_database_default(self):
+        app_label = "test_alfladd"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AlterField(
+            "Pony", "weight", models.FloatField(db_default=4.5)
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        old_weight = project_state.models[app_label, "pony"].fields["weight"]
+        self.assertIs(old_weight.db_default, models.NOT_PROVIDED)
+        new_weight = new_state.models[app_label, "pony"].fields["weight"]
+        self.assertEqual(new_weight.db_default, Value(4.5))
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            project_state.apps.get_model(app_label, "pony").objects.create()
+        # Alter field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        pony = new_state.apps.get_model(app_label, "pony").objects.create()
+        if not connection.features.can_return_columns_from_insert:
+            pony.refresh_from_db()
+        self.assertEqual(pony.weight, 4.5)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            project_state.apps.get_model(app_label, "pony").objects.create()
+        # Deconstruction.
+        definition = operation.deconstruct()
+        self.assertEqual(definition[0], "AlterField")
+        self.assertEqual(definition[1], [])
+        self.assertEqual(
+            definition[2],
+            {
+                "field": new_weight,
+                "model_name": "Pony",
+                "name": "weight",
+            },
+        )
+
+    def test_alter_field_change_default_to_database_default(self):
+        """The AlterField operation changing default to db_default."""
+        app_label = "test_alflcdtdd"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AlterField(
+            "Pony", "pink", models.IntegerField(db_default=4)
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        old_pink = project_state.models[app_label, "pony"].fields["pink"]
+        self.assertEqual(old_pink.default, 3)
+        self.assertIs(old_pink.db_default, models.NOT_PROVIDED)
+        new_pink = new_state.models[app_label, "pony"].fields["pink"]
+        self.assertIs(new_pink.default, models.NOT_PROVIDED)
+        self.assertEqual(new_pink.db_default, Value(4))
+        pony = project_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        self.assertEqual(pony.pink, 3)
+        # Alter field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        pony = new_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        if not connection.features.can_return_columns_from_insert:
+            pony.refresh_from_db()
+        self.assertEqual(pony.pink, 4)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        pony = project_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        self.assertEqual(pony.pink, 3)
+
+    def test_alter_field_change_nullable_to_database_default_not_null(self):
+        """
+        The AlterField operation changing a null field to db_default.
+        """
+        app_label = "test_alflcntddnn"
+        project_state = self.set_up_test_model(app_label)
+        operation = migrations.AlterField(
+            "Pony", "green", models.IntegerField(db_default=4)
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        old_green = project_state.models[app_label, "pony"].fields["green"]
+        self.assertIs(old_green.db_default, models.NOT_PROVIDED)
+        new_green = new_state.models[app_label, "pony"].fields["green"]
+        self.assertEqual(new_green.db_default, Value(4))
+        old_pony = project_state.apps.get_model(app_label, "pony").objects.create(
+            weight=1
+        )
+        self.assertIsNone(old_pony.green)
+        # Alter field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        old_pony.refresh_from_db()
+        self.assertEqual(old_pony.green, 4)
+        pony = new_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        if not connection.features.can_return_columns_from_insert:
+            pony.refresh_from_db()
+        self.assertEqual(pony.green, 4)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        pony = project_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        self.assertIsNone(pony.green)
+
+    @skipIfDBFeature("interprets_empty_strings_as_nulls")
+    def test_alter_field_change_blank_nullable_database_default_to_not_null(self):
+        app_label = "test_alflcbnddnn"
+        table_name = f"{app_label}_pony"
+        project_state = self.set_up_test_model(app_label)
+        default = "Yellow"
+        operation = migrations.AlterField(
+            "Pony",
+            "yellow",
+            models.CharField(blank=True, db_default=default, max_length=20),
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertColumnNull(table_name, "yellow")
+        pony = project_state.apps.get_model(app_label, "pony").objects.create(
+            weight=1, yellow=None
+        )
+        self.assertIsNone(pony.yellow)
+        # Alter field.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertColumnNotNull(table_name, "yellow")
+        pony.refresh_from_db()
+        self.assertEqual(pony.yellow, default)
+        pony = new_state.apps.get_model(app_label, "pony").objects.create(weight=1)
+        if not connection.features.can_return_columns_from_insert:
+            pony.refresh_from_db()
+        self.assertEqual(pony.yellow, default)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertColumnNull(table_name, "yellow")
+        pony = project_state.apps.get_model(app_label, "pony").objects.create(
+            weight=1, yellow=None
+        )
+        self.assertIsNone(pony.yellow)
 
     def test_alter_field_add_db_column_noop(self):
         """

tests/schema/tests.py
@@ -2102,6 +2102,33 @@ class SchemaTests(TransactionTestCase):
         with self.assertRaises(IntegrityError):
             NoteRename.objects.create(detail_info=None)
 
+    @isolate_apps("schema")
+    def test_rename_keep_db_default(self):
+        """Renaming a field shouldn't affect a database default."""
+
+        class AuthorDbDefault(Model):
+            birth_year = IntegerField(db_default=1985)
+
+            class Meta:
+                app_label = "schema"
+
+        self.isolated_local_models = [AuthorDbDefault]
+        with connection.schema_editor() as editor:
+            editor.create_model(AuthorDbDefault)
+        columns = self.column_classes(AuthorDbDefault)
+        self.assertEqual(columns["birth_year"][1].default, "1985")
+
+        old_field = AuthorDbDefault._meta.get_field("birth_year")
+        new_field = IntegerField(db_default=1985)
+        new_field.set_attributes_from_name("renamed_year")
+        new_field.model = AuthorDbDefault
+        with connection.schema_editor(
+            atomic=connection.features.supports_atomic_references_rename
+        ) as editor:
+            editor.alter_field(AuthorDbDefault, old_field, new_field, strict=True)
+        columns = self.column_classes(AuthorDbDefault)
+        self.assertEqual(columns["renamed_year"][1].default, "1985")
+
     @skipUnlessDBFeature(
         "supports_column_check_constraints", "can_introspect_check_constraints"
     )

senior106/Django-Project

commit 1cb1928ef9f9976ae7d0b24ddd4cfdd229857a5c

Fixed #34946 -- Preserved db_default on combined default field addition.
Regression in 7414704e88d73dafbcfbb85f9bc54cb6111439d3.

django/db/backends/base/schema.py
@@ -744,9 +744,9 @@ class BaseDatabaseSchemaEditor:
         }
         self.execute(sql, params)
         # Drop the default if we need to
-        # (Django usually does not use in-database defaults)
         if (
-            not self.skip_default_on_alter(field)
+            field.db_default is NOT_PROVIDED
+            and not self.skip_default_on_alter(field)
             and self.effective_default(field) is not None
         ):
             changes_sql, params = self._alter_column_default_sql(

tests/migrations/test_operations.py
@@ -1692,15 +1692,22 @@ class OperationTests(OperationTestBase):
         field = new_state.models[app_label, "pony"].fields["height"]
         self.assertEqual(field.default, 3)
         self.assertEqual(field.db_default, Value(4))
-        project_state.apps.get_model(app_label, "pony").objects.create(weight=4)
+        pre_pony_pk = (
+            project_state.apps.get_model(app_label, "pony").objects.create(weight=4).pk
+        )
         self.assertColumnNotExists(table_name, "height")
         # Add field.
         with connection.schema_editor() as editor:
             operation.database_forwards(app_label, editor, project_state, new_state)
         self.assertColumnExists(table_name, "height")
+        post_pony_pk = (
+            project_state.apps.get_model(app_label, "pony").objects.create(weight=10).pk
+        )
         new_model = new_state.apps.get_model(app_label, "pony")
-        old_pony = new_model.objects.get()
-        self.assertEqual(old_pony.height, 4)
+        pre_pony = new_model.objects.get(pk=pre_pony_pk)
+        self.assertEqual(pre_pony.height, 4)
+        post_pony = new_model.objects.get(pk=post_pony_pk)
+        self.assertEqual(post_pony.height, 4)
         new_pony = new_model.objects.create(weight=5)
         if not connection.features.can_return_columns_from_insert:
             new_pony.refresh_from_db()

tests/schema/tests.py
@@ -2257,6 +2257,23 @@ class SchemaTests(TransactionTestCase):
         columns = self.column_classes(AuthorDbDefault)
         self.assertEqual(columns["renamed_year"][1].default, "1985")
 
+    @isolate_apps("schema")
+    def test_add_field_both_defaults_preserves_db_default(self):
+        class Author(Model):
+            class Meta:
+                app_label = "schema"
+
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+
+        field = IntegerField(default=1985, db_default=1988)
+        field.set_attributes_from_name("birth_year")
+        field.model = Author
+        with connection.schema_editor() as editor:
+            editor.add_field(Author, field)
+        columns = self.column_classes(Author)
+        self.assertEqual(columns["birth_year"][1].default, "1988")
+
     @skipUnlessDBFeature(
         "supports_column_check_constraints", "can_introspect_check_constraints"
     )